6ebfcad041410c8fb53f513a1220b1faa427a5e9
Your Name <obor528622@gmail.com>
2022-02-10 13:27:24 +0700

    asd

---
+++ news/models.py
@@ -0,0 +1,37 @@
+from distutils.command.upload import upload
+from pyexpat import model
+from django.db import models
+from django.forms import CharField
+
+
+
+class News(models.Model):
+    title = models.CharField(max_length=64, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(upload_to ='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано?')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория' )
+
+    def __str__(self):
+        return self.title
+        
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
+
+class Category(models.Model):
+    title = models.CharField(max_length=64, db_index=True, verbose_name='Наименование')
+
+    def __str__(self):
+        return self.title
+        
+
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
